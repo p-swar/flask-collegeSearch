c3424cfda5914163fc085a1131b6dc59b41cb122
Prachi Swar <pswar6519@bths.edu>
2020-07-27 14:39:59 -0400

    pushing the hard coded data

---
+++ model.py
@@ -0,0 +1,41 @@
+data{
+    "majors": ["Business and Management", "Nursing", "Psychology", "Biology", "Engineering", "Education", "Communication", "Finance and Accounting", "Criminal Justice", "Anthropology and Sociology", "Computer Science", "English", "Economics", "Political Science", "History", "Art", "Math"],
+    "size": ["fewer than 5,000", "5,000 - 15,000", "more than 15,000"],
+    "location": ["Manhattan", "Brooklyn", "Queens", "The Bronx", "Staten Island"],
+
+}
+
+locations{
+    "Manhattan": ["Borough of Manhattan Community College", "Guttman Community College", "Baruch College", "City College of New York", "Hunter College", "John Jay College of Criminal Justice", "Fashion Institute of Technology", "SUNY College of Optometry", "SUNY Empire State College", "ASA College", "Barnard College", "Berkeley College", "Bori", "Boricua College", "Columbia University"],
+    "Manhattan": ["Cooper Union", "DeVry University", "Helene Fuld College of Nursing", "The Juilliard School", "The King's College", "Laboratory Institute of Merchandising", "Mandl College of Allied Health", "Manhattan School of Music", "Marymount Manhattan College", "Mercy College", "Metropolitan College of New York", "The New School", "New York Institute of Technology"],
+    "Manhattan": ["New York School of Interior Design", "New York University", "Nyack College", "Pace University", "Pacific College of Oriental Medicine", "Phillips Beth Israel School of Nursing", "Rockefeller University", "School of Visual Arts", "Sotheby's Institute of Art", "Touro College", "Yeshiva University"],
+    "Brooklyn": ["Hostos Community College", "Brooklyn College", "Long Island University - Brooklyn Campus", "Medgar Evers College", "New York City College of Technology", "ASA College", "New York University", "Pratt Institute", "St. Francis College", "St. Joseph's College"],
+    "Queens": ["LaGuardia Community College", "Queensborough Community College", "Queens College", "York College", "Long Island Business Institute - Flushing", "Plaza College", "St. John's University", "Vaughn College of Aeronautics & Technology" ],
+    "The Bronx": ["Bronx Community College", "Hostos Community College", "Lehman College", "SUNY Maritime College", "College of Mount Saint Vincent", "Fordham University", "Manhattan College", "Mercy College", "Monroe College"],
+    "Staten Island": ["College of Staten Island", "Wagner College"],
+}
+
+majors{
+    "Business and Management": ["ASA College",],
+    "Nursing": ["ASA College"],
+    "Psychology": ["Barnard College",],
+    "Biology": [""],
+    "Engineering": [],
+    "Education": [],
+    "Communications": [],
+    "Finance and Accounting": [],
+    "Criminal Justice": ["ASA College", ],
+    "Anthropology and Sociology": [],
+    "Computer Science": ["ASA College"],
+    "English": [],
+    "Economics": [],
+    "Political Science": [],
+    "History": [],
+    "Art": [],
+    "Math": [],
+
+
+    
+
+    
+}
