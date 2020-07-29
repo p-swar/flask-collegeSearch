68f5d7ad47fe24e6998ce0562463ee0e7e2820a9
alejandro <alejparr26@charter.newvisions.org>
2020-07-29 09:19:25 -0400

    Changes in model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ data{
 locations{
     "Manhattan1": ["Borough of Manhattan Community College", "Guttman Community College", "Baruch College", "City College of New York", "Hunter College", "John Jay College of Criminal Justice", "Fashion Institute of Technology", "SUNY College of Optometry", "SUNY Empire State College", "ASA College", "Barnard College", "Berkeley College", "Bori", "Boricua College", "Columbia University"],
     "Manhattan2": ["Cooper Union", "DeVry University", "Helene Fuld College of Nursing", "The Juilliard School", "The King's College", "Laboratory Institute of Merchandising", "Mandl College of Allied Health", "Manhattan School of Music", "Marymount Manhattan College", "Mercy College", "Metropolitan College of New York", "The New School", "New York Institute of Technology"],
-    "Manhattan3": ["New York School of Interior Design", "New York University", "Nyack College", "Pace University", "Pacific College of Oriental Medicine", "Phillips Beth Israel School of Nursing", "Rockefeller University", "School of Visual Arts", "Sotheby's Institute of Art", "Touro College", "Yeshiva University"],
+    "Manhattan3": ["New York School of Interior Design", "New York University", "Nyack College", "Pace University", "Pacific College of Oriental Medicine", "Phillips Beth Israel School of Nursing", "School of Visual Arts", "Touro College", "Yeshiva University"],
     "Brooklyn": ["Hostos Community College", "Brooklyn College", "Long Island University - Brooklyn Campus", "Medgar Evers College", "New York City College of Technology", "ASA College", "New York University", "Pratt Institute", "St. Francis College", "St. Joseph's College"],
     "Queens": ["LaGuardia Community College", "Queensborough Community College", "Queens College", "York College", "Long Island Business Institute - Flushing", "Plaza College", "St. John's University", "Vaughn College of Aeronautics & Technology" ],
     "The Bronx": ["Bronx Community College", "Hostos Community College", "Lehman College", "SUNY Maritime College", "College of Mount Saint Vincent", "Fordham University", "Manhattan College", "Mercy College", "Monroe College"],
@@ -40,32 +40,42 @@ majors1{
 
 
 size{
-    "fewer than 5,000a": ["Guttman Community College","York College","Fashion Institute of Technology","SUNY College of Optometry","SUNY Maritime College","ASA College","Barnard College","Berkeley College","Boricua College","College of Mount Saint Vincent","Cooper Union","Helene Fuld College of Nursing","Icahn School of Medicine at Mount Sinai","The Juilliard School","The King's College"],
+    "fewer than 5,000a": ["Guttman Community College","York College","Fashion Institute of Technology","SUNY Maritime College","ASA College","Barnard College","Berkeley College","Boricua College","College of Mount Saint Vincent","Cooper Union","Helene Fuld College of Nursing","Icahn School of Medicine at Mount Sinai","The Juilliard School","The King's College"],
     "fewer than 5,000b": ["Laboratory Institute of Merchandising","Long Island Business Institute","Mandl College of Allied Health","Manhattan College","Manhattan School of Music","Marymount Manhattan College","Metropolitan College of New York","New York School of Interior Design","Nyack College","Pacific College of Oriental Medicine","Phillips Beth Israel School of Nursing","Plaza College"],
-    "fewer than 5,000c": ["Pratt Institute","Rockefeller university","School of Visual Arts","St. Francis College","St. Joseph's College","Sotheby's Institute of Art","Vaughn College of Aeronautics & Technology","Wagner College","Yeshiva University"],
+    "fewer than 5,000c": ["Pratt Institute","School of Visual Arts","St. Francis College","St. Joseph's College","Vaughn College of Aeronautics & Technology","Wagner College","Yeshiva University"],
     "5,000 - 15,000": ["Bronx Community College","Hostos Community College","College of Staten Island","Lehman College","Long Island University - Brooklyn Campus","Medgar Evers College","Empire State College","Empire State College","Columbia University","Hofstra University","Mercy College","Monroe College","The New School","New York Institute of Technology","Pace University","Touro College"],
     "more than 15,000a": ["Borough of Manhattan Community College","Kingsborough Community College","LaGuardia Community College","Queensborough Community College","Baruch College","Brooklyn College","City College of New York","Hunter College","John Jay College of Criminal Justice","New York City College of Technology","Queens College","DeVry University","Fordham University"],
-    "more than 15,000b": ["Long Island University","New York University","St. John's University"],
+    "more than 15,000b": ["Long Island University","New York University","St. John's University"]
 }
 
 majors{
-    "Business and Management": ["Borough of Manhattan Community College","Bronx Community College","Guttman Community College","Hostos Community College","","","","","","","","","","","","","","],
-    "Nursing": ["Borough of Manhattan Community College","Bronx Community College","Hostos Community College","","","","","","","","","","","","",""],
-    "Psychology": ["Borough of Manhattan Community College","Bronx Community College","","","","","","","","","","","","","",""],
-    "Biology": ["Borough of Manhattan Community College","Bronx Community College","","","","","","","","","","","","","",""],
-    "Engineering": ["Borough of Manhattan Community College","Bronx Community College","Hostos Community College","","","","","","","","","","","","",""],
-    "Education": ["Borough of Manhattan Community College","Bronx Community College","","","","","","","","","","","","","",""],
-    "Communications": ["","","","","","","","","","","","","","","",""],
-    "Finance and Accounting": ["","","","","","","","","","","","","","","",""],
-    "Criminal Justice": ["Borough of Manhattan Community College","Bronx Community College","Hostos Community College","","","","","","","","","","","","",""],
-    "Anthropology and Sociology": ["","","","","","","","","","","","","","","",""],
-    "Computer Science": ["Borough of Manhattan Community College","Bronx Community College","","","","","","","","","","","","","",""],
-    "English": ["Borough of Manhattan Community College","Bronx Community College","","","","","","","","","","","","","","",],
-    "Economics": ["Borough of Manhattan Community College","Bronx Community College","","","","","","","","","","","","","",""],
-    "Political Science":["","","","","","","","","","","","","","","",""],
-    "History": ["Borough of Manhattan Community College","","","","","","","","","","","","","","",""],
-    "Art":["Borough of Manhattan Community College","","","","","","","","","","","","","","",""],
-    "Math":["Borough of Manhattan Community College","Bronx Community College","","","","","","","","","","","","","",""],
+    "Business and Management": ["Borough of Manhattan Community College","Bronx Community College","Guttman Community College","Hostos Community College","Kingsborough Community College","LaGuardia Community College","Queensborough Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Lehman College","Medgar Evers College","Queens College"],
+    "Business and Managment2": ["York College","Yeshiva University","Wagner College","Vaughn College of Aeronautics & Technology","Empire State College","SUNY Maritime College","Touro College","St. Joseph's College","St. John's University","St. Francis College","Plaza College"],
+    "Nursing": ["Borough of Manhattan Community College","Bronx Community College","Hostos Community College","Kingsborough Community College","LaGuardia Community College","Queensborough Community College","College of Staten Island","Hunter College","Lehman College","Medgar Evers College","New York City College of Technology","Queens College","York College","Wagner College"],
+    "Nursing2": ["Empire State College","Touro College","St. Joseph's College","St. Francis College"],
+    "Psychology": ["Borough of Manhattan Community College","Bronx Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","John Jay College of Criminal Justice","Lehman College","Long Island University - Brooklyn Campus","Medgar Evers College","Queens College","York College","Yeshiva University","Wagner College"],
+    "Pyschology2": ["Empire State College","Touro College","St. Joseph's College","St. John's University","St. Francis College"],
+    "Biology": ["Borough of Manhattan Community College","Bronx Community College","Kingsborough Community College","LaGuardia Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","John Jay College of Criminal Justice","Lehman College","Long Island University - Brooklyn Campus","Medgar Evers College","Queens College"],
+    "Biology2": ["York College","Yeshiva University","Wagner College","Empire State College","Touro College","St. Joseph's College","St. John's University","St. Francis College","Plaza College"],
+    "Engineering": ["Borough of Manhattan Community College","Bronx Community College","Hostos Community College","Kingsborough Community College","LaGuardia Community College","Queensborough Community College","Baruch College","City College of New York","College of Staten Island","New York City College of Technology","Vaughn College of Aeronautics & Technology","SUNY Maritime College"],
+    "Education": ["Borough of Manhattan Community College","Bronx Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","Lehman College","Medgar Evers College","Queens College","York College","Yeshiva University","Wagner College","Empire State College","Touro College","St. Joseph's College","St. John's University","St. Francis College"],
+    "Education2": ["Pratt Institute"],
+    "Communications": ["Baruch College","Brooklyn College","City College of New York","College of Staten Island","Touro College","St. John's University","St. Francis College"],
+    "Finance and Accounting": ["Kingsborough Community College","Hostos Community College","Bronx Community College","Borough of Manhattan Community College","Queensborough Community College","Brooklyn College","College of Staten Island","Hunter College","Lehman College","Medgar Evers College","Queens College","York College","Yeshiva University","Empire State College","Touro College"],
+    "Finance and Accounting2": ["St. Joseph's College","St. John's University","St. Francis College"],
+    "Criminal Justice": ["Borough of Manhattan Community College","Bronx Community College","Hostos Community College","Kingsborough Community College","LaGuardia Community College","Queensborough Community College","John Jay College of Criminal Justice","St. Joseph's College","St. Francis College"],
+    "Anthropology and Sociology": ["Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","John Jay College of Criminal Justice","Lehman College","Queens College","York College","Yeshiva University","Wagner College","Touro College","St. Joseph's College","St. John's University","St. Francis College"],
+    "Computer Science": ["Borough of Manhattan Community College","Bronx Community College","Kingsborough Community College","LaGuardia Community College","Queensborough Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","John Jay College of Criminal Justice","Lehman College","Medgar Evers College"],
+    "Computer Science2": ["New York City College of Technology","Queens College","York College","Yeshiva University","Wagner College","St. Joseph's College","St. John's University"],
+    "English": ["Borough of Manhattan Community College","Bronx Community College","LaGuardia Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","Lehman College","Long Island University - Brooklyn Campus","Medgar Evers College","Queens College","York College","Yeshiva University","Wagner College","Empire State College"],
+    "English2": ["Touro College","St. Joseph's College","St. John's University","St. Francis College"],
+    "Economics": ["Borough of Manhattan Community College","Bronx Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","John Jay College of Criminal Justice","John Jay College of Criminal Justice","Lehman College","Medgar Evers College","Queens College","York College","Yeshiva University","Wagner College","Touro College"],
+    "Economics2": ["St. Joseph's College","St. John's University","St. Francis College"],
+    "Political Science":["Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","John Jay College of Criminal Justice","Lehman College","Long Island University - Brooklyn Campus","Queens College","York College","Yeshiva University","Wagner College","Touro College","St. Joseph's College","St. John's University","St. Francis College"],
+    "Art": ["Borough of Manhattan Community College","Kingsborough Community College","LaGuardia Community College","Queensborough Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","Lehman College","Queens College","York College","Wagner College","Fashion Institute of Technology","Empire State College"],
+    "Art2": ["St. Joseph's College","St. John's University","School of Visual Arts","Pratt Institute"],
+    "Math":["Borough of Manhattan Community College","Bronx Community College","Hostos Community College","Kingsborough Community College","Baruch College","Brooklyn College","City College of New York","College of Staten Island","Hunter College","John Jay College of Criminal Justice","Lehman College","Medgar Evers College","Queens College","York College","Yeshiva University","Wagner College"],
+    "Math2": ["Touro College","St. Joseph's College","St. John's University","St. Francis College"]
 
 
 }
